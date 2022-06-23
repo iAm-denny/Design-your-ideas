a2af0cb7a754e77f2bed530559e19b0dfeb386ec
Min La Pyae <minlapyae8@gmail.com>
2022-06-23 18:49:46 +0630

    Added Furniture Item Renderer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import JsonData from "./assets/floorplan-sample.json";
+import { itemsConfig } from "./assets/itemsConfig";
+import "./App.css";
+import Floorplan from "./components/Floorplan";
 
 function App() {
+  const [showFloor, setShowFloor] = useState(false);
+  const [furnitures, setFurnitures] = useState([]);
+
+  useEffect(() => {
+    let items = [];
+    JsonData.layout.map((data, index) => {
+      items.push({
+        points:
+          data.tag == "polygon" && data.points.replaceAll(" ", ",").split(","),
+        d: data.tag == "path" && data.d,
+        tag: data.tag,
+        id: data.id,
+        x: itemsConfig[index]?.x && itemsConfig[index].x,
+        y: itemsConfig[index]?.y && itemsConfig[index].y,
+        background:
+          data.tag == "path"
+            ? itemsConfig[6].background
+            : itemsConfig[index].background,
+        borderColor:
+          data.tag == "path"
+            ? itemsConfig[6].borderColor
+            : itemsConfig[index].borderColor,
+      });
+    });
+    setFurnitures(items);
+  }, []);
+
+  const toggleFurniture = () => {
+    setShowFloor((prev) => !prev);
+  };
+
+  const resetItems = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="load_furniture_container">
+        <button className="load_furniture" onClick={toggleFurniture}>
+          {showFloor ? "Hide" : "Show"} Furnitures
+        </button>
+        <button onClick={resetItems}>Reset Furnitures</button>
+      </div>
+      {showFloor && (
+        <div className="floorplan_container">
+          <Floorplan furnitures={furnitures} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/assets/itemsConfig.js
@@ -0,0 +1,43 @@
+export const itemsConfig = [
+  {
+    background: "#E6F1FC",
+    borderColor: "#4075FF",
+    x: 150,
+    y: 5,
+  },
+  {
+    background: "#EBFFE3",
+    borderColor: "#579903",
+    x: 450,
+    y: 5,
+  },
+  {
+    background: "#FFEDD2",
+    borderColor: "#B36B00",
+    x: 400,
+    y: 450,
+  },
+  {
+    background: "#D4D9FF",
+    borderColor: "#5C66B9",
+    x: 532,
+    y: 450,
+  },
+  {
+    background: "#FFF0F0",
+    borderColor: "#E72D35",
+    x: 10,
+    y: 580,
+  },
+  {
+    background: "#FFD1FA",
+    borderColor: "#B2009F",
+    x: 600,
+    y: 650,
+  },
+  {
+    tag: "path",
+    background: "#F5F5F5",
+    borderColor: "#000",
+  },
+];
