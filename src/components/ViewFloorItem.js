84bd2cf0e02770b46dbb89f90938ea0b3c75b7a3
Min La Pyae <minlapyae8@gmail.com>
2022-06-26 21:08:10 +0630

    Fixed that response wrong position shape

--- src/components/ViewFloorItem.js
+++ src/components/ViewFloorItem.js
@@ -13,6 +13,7 @@ const ViewFloorItem = (props) => {
   } = props;
 
   const handleDragStart = (e) => {
+    console.log("-..........", e);
     const id = e.target.attrs.name;
     const cloneFurniture = [...displayFurnitures];
     const searchItem = cloneFurniture.find((item) => item.id == id);
@@ -26,6 +27,7 @@ const ViewFloorItem = (props) => {
     const id = e.target.attrs.name;
     const x = e.target._lastPos.x;
     const y = e.target._lastPos.y;
+
     const cloneFurniture = [...displayFurnitures];
     const searchItem = cloneFurniture.find((item) => item.id == id);
     const newData = { ...searchItem, x, y };
@@ -39,46 +41,14 @@ const ViewFloorItem = (props) => {
     <Group
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
+      draggable={cursor == "hand"}
+      name={item.id}
       onClick={(e) => {
         e.cancelBubble = true;
         if (cursor == "arrow") {
           handleSelectShape(item.id);
         }
       }}
-      draggable={cursor == "hand"}
-      name={item.id}
-      x={item.x}
-      y={item.y}
-    >
-      <Line
-        points={item.points}
-        fill={item.background}
-        stroke={selectedShapeId == item.id ? "red" : item.borderColor}
-        closed
-        scale={{
-          x: 0.3,
-          y: 0.3,
-        }}
-        onMouseUp={(e) => {
-          if (cursor == "hand") {
-            const container = e.target.getStage().container();
-            container.style.cursor = "grab";
-          }
-        }}
-        onMouseDown={(e) => {
-          if (cursor == "hand") {
-            const container = e.target.getStage().container();
-            container.style.cursor = "grabbing";
-          }
-        }}
-      />
-    </Group>
-  ) : (
-    <Group
-      draggable={cursor == "hand"}
-      name={item.id}
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd}
       onMouseUp={(e) => {
         if (cursor == "hand") {
           const container = e.target.getStage().container();
@@ -91,19 +61,51 @@ const ViewFloorItem = (props) => {
           container.style.cursor = "grabbing";
         }
       }}
+      x={item.x}
+      y={item.y}
+    >
+      <Line
+        points={item.points}
+        fill={item.background}
+        stroke={selectedShapeId == item.id ? "red" : item.borderColor}
+        closed
+        scale={{
+          x: 0.3,
+          y: 0.3,
+        }}
+      />
+    </Group>
+  ) : (
+    <Group
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      draggable={cursor == "hand"}
+      name={item.id}
+      onClick={(e) => {
+        e.cancelBubble = true;
+        if (cursor == "arrow") {
+          handleSelectShape(item.id);
+        }
+      }}
+      onMouseUp={(e) => {
+        if (cursor == "hand") {
+          const container = e.target.getStage().container();
+          container.style.cursor = "grab";
+        }
+      }}
+      onMouseDown={(e) => {
+        if (cursor == "hand") {
+          const container = e.target.getStage().container();
+          container.style.cursor = "grabbing";
+        }
+      }}
+      x={item.x}
+      y={item.y}
     >
       <Path
         data={item.d}
         fill={item.background}
         stroke={selectedShapeId == item.id ? "red" : item.borderColor}
-        onClick={(e) => {
-          e.cancelBubble = true;
-          if (cursor == "arrow") {
-            handleSelectShape(item.id);
-          }
-        }}
-        x={item.x}
-        y={item.y}
         strokeWidth={8}
         scale={{
           x: 0.4,
