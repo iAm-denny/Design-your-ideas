a4978079f18fb2fbbce3c0086228824edb77584c
Min La Pyae <minlapyae8@gmail.com>
2022-06-26 16:49:19 +0630

    Modified Clean codes, Added landing screen & Example page

--- src/components/FloorplanList.js
+++ src/components/FloorplanList.js
@@ -2,26 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Layer, Stage } from "react-konva";
 import ViewFloorItem from "./ViewFloorItem";
 
-const FloorplanList = ({ displayFurnitures, setDisplayFurnitures }) => {
-  const [isSelect, setIsSelect] = useState(false);
-  const [selectedId, setSelectedId] = useState(null);
-
-  const handleClick = (id) => {
-    setIsSelect(true);
-    setSelectedId(id);
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Backspace" && isSelect && selectedId) {
-        let cloneFurniture = displayFurnitures;
-
-        let data = cloneFurniture.filter((c) => c.id != selectedId);
-
-        setDisplayFurnitures(data);
-      }
-    });
-  }, [selectedId]);
+const FloorplanList = (props) => {
+  const {
+    displayFurnitures,
+    setDisplayFurnitures,
+    selectedShapeId,
+    setIsSelectShape,
+    handleSelectShape,
+  } = props;
 
   return (
     <div>
@@ -29,27 +17,28 @@ const FloorplanList = ({ displayFurnitures, setDisplayFurnitures }) => {
         width={window.innerWidth}
         height={window.innerHeight - 60}
         draggable
-        zIndex={-1}
-        onClick={(e) => {
-          e.isTrusted = false;
-          setIsSelect(false);
-          setSelectedId(null);
+        style={{ cursor: "grab" }}
+        onMouseDown={(e) => {
+          const container = e.target.getStage().container();
+          container.style.cursor = "grabbing";
+        }}
+        onMouseUp={(e) => {
+          const container = e.target.getStage().container();
+          container.style.cursor = "grab";
+        }}
+        onClick={() => {
+          setIsSelectShape(false);
+          handleSelectShape(null);
         }}
       >
         {displayFurnitures.map((item) => (
-          <Layer
-            style={{
-              cursor: "move",
-              border: "1px solid red",
-            }}
-            key={item.id}
-          >
+          <Layer key={item.id}>
             <ViewFloorItem
               item={item}
               displayFurnitures={displayFurnitures}
               setDisplayFurnitures={setDisplayFurnitures}
-              handleClick={handleClick}
-              selectedId={selectedId}
+              handleSelectShape={handleSelectShape}
+              selectedShapeId={selectedShapeId}
             />
           </Layer>
         ))}
